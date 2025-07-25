d4cc0bb515084a4c2c8e225fae99338296bb35d8
Christian <palmerc@udel.edu>
2025-07-24 20:28:27 -0400

    Added a simple future value calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
+
+
 function App() {
+  
+  
+  const [futureValue, setFutureValue] = useState<number>(0);
+  const [presentValue, setPresentValue] = useState<number>(0);
+  const [numPeriods, setnumPeriods] = useState<number>(0);
+  const [interestRate, setinterestRate] = useState<number>(0);
+  
+
+  function calculateFutureValue(): void{
+    setFutureValue(presentValue*(1+(interestRate/100))**numPeriods);
+  }
+
+  const handlePresentValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPresentValue(Number(event.target.value));  // Update state with input's current value
+  };
+
+   const handleInterestRateChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setinterestRate(Number(event.target.value));  // Update state with input's current value
+  }; 
+  
+  const handleNumPeriodsChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setnumPeriods(Number(event.target.value));  // Update state with input's current value
+  };
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Hello
-        </a>
-      </header>
+      <div>
+        Calculate Future Value 
+      </div>
+      <input type="number" placeholder="Enter the Present Value Here" onChange={handlePresentValueChange} />
+      <input type="number"placeholder = "interest Rate %" onChange={handleInterestRateChange} />
+      <input type="number"placeholder = "# periods" onChange={handleNumPeriodsChange} />
+      <button onClick={calculateFutureValue}>Calculate Future Value</button>
+      <div>{futureValue}</div>
     </div>
   );
 }
